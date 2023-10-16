b6e85127e3d1ce2aea6e57dc3ea98ead5802a04c
Jackson Westbrook <infamousjackson19@gmail.com>
2023-10-16 17:45:46 -0400

    Flipped Direction

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.onmousemove = e => {
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
 
-    const percentage = (mouseDelta / maxDelta) * 100;
+    const percentage = (mouseDelta / maxDelta) * -100;
 
     track.style.transform = 'translate(${percentage}%, -50%)';
 }
