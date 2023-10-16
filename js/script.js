4afd83f4c40874414555b72b445dc115f5a0608d
Jackson Westbrook <infamousjackson19@gmail.com>
2023-10-16 17:45:24 -0400

    Added sliding images

--- js/script.js
+++ js/script.js
@@ -4,9 +4,17 @@ window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
 }
 
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+}
+
 window.onmousemove = e => {
+    if (track.dataset.mouseDownAt == "0") return;
+
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
         maxDelta = window.innerWidth / 2;
 
     const percentage = (mouseDelta / maxDelta) * 100;
+
+    track.style.transform = 'translate(${percentage}%, -50%)';
 }
